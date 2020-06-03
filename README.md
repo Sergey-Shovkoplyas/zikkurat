237174fec02b9d8d701bb66febb6944c1ccdf227
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-06-03 15:26:31 +0300

    Add home.html

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Start template
+# zikkurat
 
 Start template with (optional)
 - normalize.css
--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,29 @@
-window.onload = function () {
-	
-	console.log('js is working!!!'); 
+$( document ).ready(function() {
 
-}
+	// ------------------- section-slider itialize ----------------
 
-/* $( document ).ready(function() {
+	$('.home__slider-1 .sec-slider__slider-wrap').slick({
+		variableWidth: true,
+		slidesToShow: 6,
+		slidesToScroll: 6,
+		prevArrow: $('.home__slider-1 .sec-slider__arrow.prev'),
+		nextArrow: $('.home__slider-1 .sec-slider__arrow.next')
+	});
 
-	console.log( 'JQuery!!!' );
+	$('.home__slider-2 .sec-slider__slider-wrap').slick({
+		variableWidth: true,
+		slidesToShow: 6,
+		slidesToScroll: 6,
+		prevArrow: $('.home__slider-2 .sec-slider__arrow.prev'),
+		nextArrow: $('.home__slider-2 .sec-slider__arrow.next')
+	});
 
-}); */
+	$('.home__slider-3 .sec-slider__slider-wrap').slick({
+		variableWidth: true,
+		slidesToShow: 6,
+		slidesToScroll: 6,
+		prevArrow: $('.home__slider-3 .sec-slider__arrow.prev'),
+		nextArrow: $('.home__slider-3 .sec-slider__arrow.next')
+	});
+
+});
